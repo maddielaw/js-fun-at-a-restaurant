09bb029d08dd18fcddbcaec6fd62afea06140576
Madison Law <madisonlaw1@gmail.com>
2021-12-01 20:34:12 -0700

    Add meal exercise and tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`
+};
 
-}
+function createMenuItem(name, price, type) {
+    var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  };
+  return menuItem;
+};
+
+function addIngredients(ingredient, ingredientsList) {
+    if (ingredientsList.includes(ingredient) === false) {
+      return ingredientsList.push(ingredient)
+    };
+};
+
+function formatPrice(initialPrice, formattedPrice) {
+  return formattedPrice = `$${initialPrice}`
+};
+
+function decreasePrice(menuItemPrice) {
+   return menuItemPrice - (menuItemPrice / 10);
+};
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+  return recipe;
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
