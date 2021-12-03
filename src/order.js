f2635efb7db138f30836981dbc7e6b945a5ca16a
Madison Law <madisonlaw1@gmail.com>
2021-12-02 20:21:46 -0700

    Add takeOrder, refundOrder, and listItems tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,35 @@
 
+function takeOrder(newOrder, deliveryOrders) {
+  if (deliveryOrders.length <= 2) {
+    return deliveryOrders.push(newOrder);
+  } 
+};
+
+function refundOrder(orderID, deliveryOrders) {
+  for (i = 0; i < deliveryOrders.length; i ++) {
+    if (deliveryOrders[i].orderNumber === orderID) {
+      return deliveryOrders.splice(i, 1)
+    }
+  }
+};
+
+function listItems(deliveryOrders) {
+  var itemList = [];
+  for (i= 0; i < deliveryOrders.length; i ++) {
+    itemList.push(deliveryOrders[i].item);
+  }
+  return itemList.join(", ");
+};
+
+function searchOrder() {
+
+};
+
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
