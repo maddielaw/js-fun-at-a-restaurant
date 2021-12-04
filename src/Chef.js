7c10daaa895cfde322960e5f7f876a04670e0e86
Madison Law <madisonlaw1@gmail.com>
2021-12-04 13:23:51 -0700

    Add Chef tests for setup, name, restaurant, greeting

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,15 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+     this.name = name;
+     this.restaurant = restaurant;
   }
-}
+  greetCustomer(customerName, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${customerName}!`
+    } else {
+    return `Hello, ${customerName}!`;
+    }
+  }
+};
 
 module.exports = Chef;
