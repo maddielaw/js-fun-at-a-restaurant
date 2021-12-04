133d6c0a3999e6fcf7c415008cff0dd15518dc94
Madison Law <madisonlaw1@gmail.com>
2021-12-04 11:06:13 -0700

    Refactor restaurant tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,37 +1,37 @@
 
-function createRestaurant(name, menus) {
-  var newRestaurant = {
-    name: name,
-    menus: {
+function createRestaurant(name) {
+    var newRestaurant = {
+      name: name,
+      menus: {
       breakfast: [],
       lunch: [],
       dinner: []
-    }
-  };
-  return newRestaurant;
+      }
+    };
+    return newRestaurant;
 };
 
 
 
 function addMenuItem(newRestaurant, newMenuItem) {
-  for (i = 0; i < newRestaurant.menus[newMenuItem.type].length; i ++) {
-    if (newRestaurant.menus[newMenuItem.type][i].name === newMenuItem.name) {
-      return 
-    } 
-  };
-  return newRestaurant.menus[newMenuItem.type].push(newMenuItem)
+    for (i = 0; i < newRestaurant.menus[newMenuItem.type].length; i ++) {
+      if (newRestaurant.menus[newMenuItem.type][i].name === newMenuItem.name) {
+        return 
+      } 
+    };
+    return newRestaurant.menus[newMenuItem.type].push(newMenuItem)
 };
 
 
 function removeMenuItem(newRestaurant, item, type) {
-  var array = newRestaurant.menus[type];
-  for (i = 0; i < array.length; i ++) {
-    if (array[i].name === item) {
+    var menuTypes = newRestaurant.menus[type];
+      for (i = 0; i < menuTypes.length; i ++) {
+      if (menuTypes[i].name === item) {
       newRestaurant.menus[type].splice(i, 1);
       return `No one is eating our ${item} - it has been removed from the ${type} menu!`
-    } 
-  }
-  return `Sorry, we don't sell ${item}, try adding a new recipe!`
+      } 
+    }
+    return `Sorry, we don't sell ${item}, try adding a new recipe!`
 };
 
 
